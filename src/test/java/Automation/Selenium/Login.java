cc15d0325d24275dadebb3c32f45f32eee6a1730
Priya <priyambada.moharana1@gmail.com>
2020-12-02 20:32:02 +0530

    Initial version

---
+++ src/test/java/Automation/Selenium/Login.java
@@ -0,0 +1,25 @@
+package Automation.Selenium;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	static int a;
+	static
+	{
+		a =5;
+		System.out.println("Value of a is : " + a);
+	}
+
+public static void main(String args[])
+{
+	System.out.println("Program for git");
+}
+@Test
+public void test()
+{
+	Assert.assertTrue(true);
+}
+
+}
